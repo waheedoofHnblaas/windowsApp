c168d63571b01a02430c896c9be4b20f3e481e0e
waheed hanblaas <www.waheed.hanblaas@gmail.com>
2023-10-07 15:03:31 +0300

    add all 11

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,14 +3,14 @@ class AppDims {
   static double smallWidth = 100;
   static double smallHeight = 100;
 
-  //=============  SHOW TEXT FIELD   ============
+  //=============  SHOW TEXT FIELD  ============
   static double width = 600;
   static double height = 100;
 
-  //=============  TEXT FIELD   ============
+  //=============  TEXT FIELD    ============
   static double textFieldWidth = 500;
   static double textFieldHeight = 200;
-  //=============  Chat  ============
+  //=============   Chat   ============
   static double chatWidth = 500;
   static double chatHeight = 400;
 }
